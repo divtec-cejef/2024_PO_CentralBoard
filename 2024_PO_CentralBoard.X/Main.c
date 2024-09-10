aae09ab11154bdc6dd4912135ab1c8297897aa4a
cristhian <cristhian.penavalencia@divtec.ch>
2024-09-10 08:32:56 +0200

    version Cristhian

--- 2024_PO_CentralBoard.X/Main.c
+++ 2024_PO_CentralBoard.X/Main.c
@@ -1,266 +1,146 @@
-/******************************************************************************/
-/*                                                                            */
-/*                                CEJEF                                       */
-/*                          Division Technique                                */
-/*                            EMT Porrentruy                                  */
-/*                                                                            */
 /*----------------------------------------------------------------------------*/
-/*                                                                            */
-/* Project     : lecteur MP3                                                  */
-/* Author      : Pena                                         */
+/* Project     : programmeCircuit                                             */
+/* Author      : Cristhian Valencia                                           */
 /* Target      : PIC 18F47Q10                                                 */
-/*                                                                            */
-/* File name   : Main.c                                                       */
-/* Description : Gestion de la carte centrale du circuit                      */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/* IDE version : MPLAB X IDE v6.0                                             */
-/* Compiler v. : CCS C Compiler v5.116                                        */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/* Historique  :                                                              */
-/*                                                                            */
-/* <date>     <name>      <rev>   <description>                               */
-/* 03.09.24    CHF         1.0    First version                               */
-/*                                                                            */
-/******************************************************************************/
-/*----------------------------------------------------------------------------*/
-/*                          INCLUDES                                          */
 /*----------------------------------------------------------------------------*/
 
 #include "18F47Q10.h"
-#fuses RSTOSC_HFINTRC_64MHZ, NOWDT, NOPUT, NOBROWNOUT, LVP, NOCLKOUT, NOEXTOSC
-//#fuses RSTOSC_HFINTRC_64MHZ, NOWDT, NOPUT, NOBROWNOUT, NOCLKOUT
+#fuses RSTOSC_HFINTRC_64MHZ, NOWDT, NOPUT, NOBROWNOUT, LVP, NOCLKOUT
 #use delay (clock=64000000)
-//#use RS232(UART1, baud=9600, ERRORS, stream=STR1)
-//#use RS232(UART1, baud=9600)
-#use rs232(baud=9600, xmit=PIN_B2, rcv=PIN_B1, stream=MUSIC)
 
 #include "MiniLCD_SPI_2x16v2.c"
-#include "DFPlayer.c"
 
-/*----------------------------------------------------------------------------*/
-/*                          LOCAL VARIABLES                                   */
-/*----------------------------------------------------------------------------*/
-int Tab[10] = {1,3,5,7,10,15,12,1,8,9};
-int i, numTrack=0;
-int IndexMin;
-int IndexMax;
-int TabMin = Tab[0];
-int TabMax = Tab[0];
-int temp =0, counter = 0;
+//==============================================================================
+//définition des entrées
+//==============================================================================
 
-/*----------------------------------------------------------------------------*/
-/*                          FUNCTIONS                                         */
-/*----------------------------------------------------------------------------*/
-//unsigned int8 send_buf[9];
-int8 state=0;
-/*
-void play_1(unsigned int8 cmd, unsigned int8 feedback);
-void volume_max(unsigned int8 cmd, unsigned int8 feedback);
-void DFPlayer_NextSong();
-void DFPlayer_PlaySongNb(int numSong);
-void DFPlayer_Pause();
-void DFPlayer_SetVolume(int volume);
-void DFPlayer_PlayBack();*/
-void init();
+#define BUZZER  PIN_B4 
+
+//==============================================================================
+//définition des sorties
+//==============================================================================
+
+#define LEDD_1  PIN_A4
+#define LEDD_2  PIN_A5
+#define LEDD_3  PIN_A6
+
+//==============================================================================
+//définition des états
+//==============================================================================
+
+#define STARTING    1
+#define WAIT        2
+#define READY       3
+#define COUNTDOWN   4
+#define RUN         5
+#define TIME        6
+#define ENDING      7
+
+//==============================================================================
+//Interruption timer
+//==============================================================================
+
+int count = 0;
 
 #INT_TIMER0
 void Timer0_ISR()
 {
-    //set_timer0(TIMER0_2S);
-    output_toggle(PIN_A4);
-    
+    set_timer0(25536);
+    count++;
 }
 
-void main(){ 
-    init();
-    while(1)
+//==============================================================================
+//Programme principal
+//==============================================================================
+
+void main()
+{
+
+    //== Déclaration des variables ==//                                               
+    int8 state = 0;     // État //     
+    int8 timer = 0;     // Chrono // 
+    int8 blink = 0;     // Animation - leds clignotent - O X O - X O X //
+    int8 secon = 0;     // Secondes //
+    int8 dseco = 0;     // Dixièmes de secondes //
+    
+    //== Entrées ==//                            
+    int8 buzzr = 0;     // Bouton buzzer principal //
+    
+    //== Sorties ==//
+    int8 lled1 = 0;     // 3 Feux //
+    int8 lled2 = 0;
+    int8 lled3 = 0;
+    
+    //== Flancs descendants ==//                                                         
+    int8 prBzr = 0;     // Pour faire l'antirebond du Buzzer //
+     
+    //== Initialisation ==//   
+    state = STARTING;   
+    
+    //== Boucle infinie ==//   
+    while(true)
     {
-        //output_toggle(PIN_D1);
-        if(!input(PIN_B4))
-        {
-            //volume_max(0x06,0);
-            DFPlayer_PlaySongNb(numTrack);
-            delay_ms(1000);
-            numTrack++;
-            //play_1(0x01,0);
-        }
-        if(!input(PIN_C5))
-        {
-            if(state == 0)
-            {
-                output_toggle(PIN_A6);
-                //play_1(0x05,0);
-                //DFPlayer_NextSong();
-                DFPlayer_Pause();
-                
-                state =1;
-            }
-            else if (state == 1)
-            {
-                output_toggle(PIN_A6);
-                DFPlayer_PlayBack();
-                state = 0;
-            }
-        }
-        delay_ms(200);
         
-        output_toggle(PIN_A5);
-    }
-   
-}
-void init(){
-   //setup_timer_0(T0_INTERNAL);
-   //enable_interrupts(INT_TIMER0);
-   //enable_interrupts(GLOBAL);
-    DFPlayer_SetVolume(10);
-   
-}
-/*
-void volume_max(unsigned int8 cmd, unsigned int8 feedback){
-   unsigned int8 i;
-   send_buf[0] = 0x7E;           // Start byte
-   send_buf[1] = 0xFF;           // Version
-   send_buf[2] = 0x06;           // Len
-   send_buf[3] = cmd;            // Command, see 3.2
-   send_buf[4] = feedback;       // Feedback, always 0 => 0
-   send_buf[5] = 0x00;           // High byte (DH), para 1
-   send_buf[6] = 0x1E;           // Low byte (DL), para 2
-//!   send_buf[7] = 0xFF;           // checksum highbyte
-//!   send_buf[8] = 0xD5;           // checksum low byte
-   send_buf[7] = 0xEF;           // end
-   send_buf[8] = '\0';
-   for(i = 0; i < 8; i ++){
-      printf("%c",send_buf[i]);
-   };
-}
+        //== Lecture des entrées ==//        
+        buzzr = input(BUZZER);
+        
+        //== Lecture des états ==//
+        switch (state)
+        {
+            case STARTING:
+                
+                break;
 
-void play_1(unsigned int8 cmd, unsigned int8 feedback){
-   unsigned int8 i;
-   send_buf[0] = 0x7E;           // Start byte
-   send_buf[1] = 0xFF;           // Version
-   send_buf[2] = 0x06;           // Len
-   send_buf[3] = cmd;            // Command, see 3.2
-   send_buf[4] = feedback;       // Feedback, always 0 => 0
-   send_buf[5] = 0x00;           // High byte (DH), para 1
-   send_buf[6] = 0x01;           // Low byte (DL), para 2
-//!   send_buf[7] = 0xFF;           // checksum highbyte
-//!   send_buf[8] = 0xE6;           // checksum low byte
-   send_buf[7] = 0xEF;           // end
-   
-   for(i = 0; i < 8; i ++){
-      printf("%c",send_buf[i]);
-      //putc(send_buf[i]);
-   };
-   
-  
-   //puts(send_buf);
-   
-}
-
-//Attention l'ordre des chansons est donné par l'ordre dans lequel vous les 
-//copiez
-
-
-void DFPlayer_NextSong()
-{
-   unsigned int8 i;
-   send_buf[0] = 0x7E;              // Start byte
-   send_buf[1] = 0xFF;              // Version
-   send_buf[2] = 0x06;              // Len
-   send_buf[3] = 0x01;              // Command 0X01 Next
-   send_buf[4] = 0x00;              // Feedback, always 0 => 0
-   send_buf[5] = 0x00;              // High byte (DH), para 1
-   send_buf[6] = 0x00;              // Low byte (DL), para 2
-//!   send_buf[7] = 0xFF;           // checksum highbyte
-//!   send_buf[8] = 0xE6;           // checksum low byte
-   send_buf[7] = 0xEF;              // end
-   
-   for(i = 0; i < 8; i ++){
-      printf("%c",send_buf[i]);
-      //putc(send_buf[i]);
-   }; 
-}
-
-void DFPlayer_SetVolume(int volume)
-{
-   unsigned int8 i;
-   send_buf[0] = 0x7E;              // Start byte
-   send_buf[1] = 0xFF;              // Version
-   send_buf[2] = 0x06;              // Len
-   send_buf[3] = 0x06;              // Command 0X01 Next
-   send_buf[4] = 0x00;              // Feedback, always 0 => 0
-   send_buf[5] = 0x00;              // High byte (DH), para 1
-   send_buf[6] = volume;              // Low byte (DL), para 2
-//!   send_buf[7] = 0xFF;           // checksum highbyte
-//!   send_buf[8] = 0xE6;           // checksum low byte
-   send_buf[7] = 0xEF;              // end
-   
-   for(i = 0; i < 8; i ++){
-      printf("%c",send_buf[i]);
-      //putc(send_buf[i]);
-   }; 
-}
-void DFPlayer_Pause()
-{
-   unsigned int8 i;
-   send_buf[0] = 0x7E;              // Start byte
-   send_buf[1] = 0xFF;              // Version
-   send_buf[2] = 0x06;              // Len
-   send_buf[3] = 0x0E;              // Command, 0x0E Pause
-   send_buf[4] = 0x00;              // Feedback, always 0 => 0
-   send_buf[5] = 0x00;              // High byte (DH), para 1
-   send_buf[6] = 0x00;              // Low byte (DL), para 2
-//!   send_buf[7] = 0xFF;           // checksum highbyte
-//!   send_buf[8] = 0xE6;           // checksum low byte
-   send_buf[7] = 0xEF;              // end
-   
-   for(i = 0; i < 8; i ++){
-      printf("%c",send_buf[i]);
-      //putc(send_buf[i]);
-   };
-}
-void DFPlayer_PlayBack()
-{
-   unsigned int8 i;
-   send_buf[0] = 0x7E;              // Start byte
-   send_buf[1] = 0xFF;              // Version
-   send_buf[2] = 0x06;              // Len
-   send_buf[3] = 0x0D;              // Command, 0x0E Playback
-   send_buf[4] = 0x00;              // Feedback, always 0 => 0
-   send_buf[5] = 0x00;              // High byte (DH), para 1
-   send_buf[6] = 0x00;              // Low byte (DL), para 2
-//!   send_buf[7] = 0xFF;           // checksum highbyte
-//!   send_buf[8] = 0xE6;           // checksum low byte
-   send_buf[7] = 0xEF;              // end
-   
-   for(i = 0; i < 8; i ++){
-      printf("%c",send_buf[i]);
-      //putc(send_buf[i]);
-   };
-}
-
-void DFPlayer_PlaySongNb(int numSong)
-{
-   unsigned int8 i;
-   send_buf[0] = 0x7E;           // Start byte
-   send_buf[1] = 0xFF;           // Version
-   send_buf[2] = 0x06;           // Len
-   send_buf[3] = 0x03;            // Command, see 3.2
-   send_buf[4] = 0x00;       // Feedback, always 0 => 0
-   send_buf[5] = 0x00;           // High byte (DH), para 1
-   send_buf[6] = numSong;           // Low byte (DL), para 2
-//!   send_buf[7] = 0xFF;           // checksum highbyte
-//!   send_buf[8] = 0xE6;           // checksum low byte
-   send_buf[7] = 0xEF;           // end
-   
-   for(i = 0; i < 8; i ++){
-      printf("%c",send_buf[i]);
-      //putc(send_buf[i]);
-   };
+//============================================================================//
+                
+            case WAIT:
+                
+                break;
+            
+//============================================================================//                
+          
+            case READY:
+                
+                break;
+            
+//============================================================================//                
+            
+            case COUNTDOWN:
+                
+                break;
+            
+//============================================================================//                
+         
+            case RUN:
+                
+                break;
+            
+//============================================================================//                
+            
+            case TIME:
+                
+                break;
+            
+//============================================================================//                
+            
+            case ENDING:
+                
+                break;
+            
+//============================================================================//
+            
+            
+        }
+        
+    }        
+                                                            
+    //== Flancs descendants ==//           
+     
+    prBzr = buzzr;
     
+        //== Écriture des sorties ==//               
+        output_bit(LEDD_1, lled1);
+        output_bit(LEDD_2, lled2);
+        output_bit(LEDD_3, lled3);
+        
 }
-*/
